c52b7ad3f83ab9e455f83c80a6c502f4ccb6964e
Nico Rithner <nicorithner@gmail.com>
2020-07-14 10:40:43 -0600

    Add building can account for renters

--- lib/building.rb
+++ lib/building.rb
@@ -2,10 +2,21 @@ class Building
     attr_reader :units, :renters
     def initialize(units= [], renters= [])
         @units = units
-        @renters = renters
+        @renters =  renters
     end
 
     def add_unit(unit)
         @units << unit
     end
+
+    def renters
+        @units.map do |unit|
+            unless unit.renter.name == nil
+                name = unit.renter.name
+                return @renters << name
+            end
+        end
+    end
 end
+
+# building.units[0].renter.name
--- test/building_test.rb
+++ test/building_test.rb
@@ -3,6 +3,7 @@ require 'minitest/pride'
 require './lib/renter'
 require './lib/apartment'
 require './lib/building'
+require "pry"
 
 class BuildingTest < Minitest::Test
 
@@ -20,6 +21,7 @@ class BuildingTest < Minitest::Test
 
   def test_it_can_add_units
     building = Building.new
+
     unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
     unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
     
@@ -33,5 +35,20 @@ class BuildingTest < Minitest::Test
     building = Building.new
 
     assert_equal [] , building.renters
+  end
+
+  def test_building_can_account_for_renters
+    building = Building.new
+
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+        
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+
+    renter1 = Renter.new("Aurora")
+    unit1.add_renter(renter1)
+
+    assert_equal ["Aurora"] , building.renters
   end
 end
